58899d7834a2b22a484f4992dd9990d72e6047cc
Edwin Joshua <edwinjoshuajohn555@gmail.com>
2026-02-25 15:03:56 -0600

    edited app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/PhoneFinder">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/quiz" element={<Quiz />} />
